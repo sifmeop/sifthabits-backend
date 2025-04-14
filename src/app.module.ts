2787273a13a350fb9801a240ba56839a16eaf0bc
Eugene <sifmeop@gmail.com>
2025-04-14 11:20:44 +0300

    add health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,23 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
 import { ScheduleModule } from '@nestjs/schedule'
+import { AppController } from './app.controller'
 import { AuthGuard } from './common/guards/auth.guard'
 import { HabitsModule } from './habits/habits.module'
 import { PrismaModule } from './prisma/prisma.module'
+import { StatisticsModule } from './statistics/statistics.module'
 import { TelegramModule } from './telegram/telegram.module'
-import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ScheduleModule.forRoot(), TelegramModule, PrismaModule, HabitsModule, StatisticsModule],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
+    TelegramModule,
+    PrismaModule,
+    HabitsModule,
+    StatisticsModule
+  ],
+  controllers: [AppController],
   providers: [
     {
       provide: APP_GUARD,
